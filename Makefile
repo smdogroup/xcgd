07ae2d16b33de7ab5ff7100620bf7b9f87f2631e
Yicong Fu <fuyicong1996@gmail.com>
2024-02-07 16:45:13 -0500

    organize files

--- Makefile
+++
@@ -1,5 +0,0 @@
-A2D_INCLUDE=${HOME}/git/a2d/include
-
-default:
-	${CXX} -std=c++17 -g -I${A2D_INCLUDE} test.cpp -o test
-	${CXX} -std=c++17 -g -I${A2D_INCLUDE} derivatives_verification.cpp -o derivatives_verification
--- examples/test.cpp
+++ examples/test.cpp
@@ -1,13 +1,12 @@
-#include <complex>
 #include <string>
 
 #include "analysis.h"
 #include "mesh.h"
-#include "physics.h"
+#include "physics/neohookean.h"
 #include "tetrahedral.h"
 
 int main(int argc, char *argv[]) {
-  using T = std::complex<double>;
+  using T = double;
   using Basis = TetrahedralBasis;
   using Quadrature = TetrahedralQuadrature;
   using Physics = NeohookeanPhysics<T>;
@@ -29,15 +28,11 @@ int main(int argc, char *argv[]) {
   T *res = new T[ndof];
   T *Jp = new T[ndof];
   T *direction = new T[ndof];
-  double *p = new double[ndof];
-  double h = 1e-30;
   for (int i = 0; i < ndof; i++) {
-    direction[i] = (double)rand() / RAND_MAX;
-    p[i] = (double)rand() / RAND_MAX;
     dof[i] = 0.01 * rand() / RAND_MAX;
-    dof[i] += T(0.0, h * direction[i].real());
     res[i] = 0.0;
     Jp[i] = 0.0;
+    direction[i] = 1.0;
   }
 
   // Allocate the physics
@@ -52,28 +47,6 @@ int main(int argc, char *argv[]) {
                              direction, Jp);
 
   std::cout << energy << std::endl;
-
-  double dres_cs = energy.imag() / h;
-  double dres_exact = 0.0;
-  double dJp_cs = 0.0;
-  double dJp_exact = 0.0;
-  for (int i = 0; i < ndof; i++) {
-    dres_exact += res[i].real() * direction[i].real();
-    dJp_cs += p[i] * res[i].imag() / h;
-    dJp_exact += Jp[i].real() * p[i];
-  }
-
-  std::printf("\nDerivatives check for the residual\n");
-  std::printf("complex step derivatives: %25.15e\n", dres_cs);
-  std::printf("exact derivatives:        %25.15e\n", dres_exact);
-  std::printf("relative error:           %25.15e\n",
-              (dres_exact - dres_cs) / dres_cs);
-
-  std::printf("\nDerivatives check for the Jacobian-vector product\n");
-  std::printf("complex step derivatives: %25.15e\n", dJp_cs);
-  std::printf("exact derivatives:        %25.15e\n", dJp_exact);
-  std::printf("relative error:           %25.15e\n",
-              (dJp_exact - dJp_cs) / dJp_cs);
 
   return 0;
 }
