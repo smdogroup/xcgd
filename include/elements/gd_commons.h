7e33233e9a70c7d27aeddef8403d7c517cb0a551
Yicong Fu <fuyicong1996@gmail.com>
2024-03-17 21:44:30 -0400

    WIP: GDMesh with LSF

--- include/elements/gd_commons.h
+++ include/elements/gd_commons.h
@@ -1,14 +1,19 @@
 #ifndef XCGD_GD_COMMONS_H
 #define XCGD_GD_COMMONS_H
 
+#include <algorithm>
+#include <set>
+#include <vector>
+
 // The structured ground grid
 template <typename T>
 class StructuredGrid2D final {
  public:
   static constexpr int spatial_dim = 2;
+  static constexpr int nverts_per_cell = 4;
 
   /**
-   * @param nxy_ numbers of elements in x, y directions
+   * @param nxy_ numbers of cells in x, y directions
    * @param lxy_ lengths of the grid in x, y directions
    * @param lxy_ origin (x, y) of the grid
    */
@@ -25,7 +30,25 @@ class StructuredGrid2D final {
     }
   }
 
-  // Compute element/vertice coordinates <-> element/vertice index
+  inline int get_num_verts() const {
+    const int* nxy = get_nxy();
+    int nnodes = 1;
+    for (int d = 0; d < spatial_dim; d++) {
+      nnodes *= nxy[d] + 1;
+    }
+    return nnodes;
+  }
+
+  inline int get_num_cells() const {
+    const int* nxy = get_nxy();
+    int nelems = 1;
+    for (int d = 0; d < spatial_dim; d++) {
+      nelems *= nxy[d];
+    }
+    return nelems;
+  }
+
+  // Compute cell/vertice coordinates <-> cell/vertice index
   inline int get_coords_vert(int ni, int nj) const {
     return ni + (nxy[0] + 1) * nj;
   }
@@ -36,29 +59,28 @@ class StructuredGrid2D final {
     nij[0] = vert % (nxy[0] + 1);
     nij[1] = vert / (nxy[0] + 1);
   }
-  inline int get_coords_elem(int ei, int ej) const { return ei + nxy[0] * ej; }
-  inline int get_coords_elem(const int* eij) const {
+  inline int get_coords_cell(int ei, int ej) const { return ei + nxy[0] * ej; }
+  inline int get_coords_cell(const int* eij) const {
     return eij[0] + nxy[0] * eij[1];
   }
-  inline void get_elem_coords(int elem, int* eij) const {
-    eij[0] = elem % nxy[0];
-    eij[1] = elem / nxy[0];
+  inline void get_cell_coords(int cell, int* eij) const {
+    eij[0] = cell % nxy[0];
+    eij[1] = cell / nxy[0];
   }
 
-  int get_elem_verts(int elem, int* verts) const {
+  void get_cell_verts(int cell, int* verts) const {
     if (verts) {
-      int nij[spatial_dim] = {elem % nxy[0], elem / nxy[1]};
+      int nij[spatial_dim] = {cell % nxy[0], cell / nxy[1]};
       verts[0] = get_coords_vert(nij);   //  3-------2
       verts[1] = verts[0] + 1;           //  |       |
       verts[2] = verts[1] + nxy[0] + 1;  //  |       |
       verts[3] = verts[2] - 1;           //  0-------1
     }
-    return 4;
   }
 
-  void get_elem_vert_ranges(int elem, T* xloc_min, T* xloc_max) const {
+  void get_cell_vert_ranges(int cell, T* xloc_min, T* xloc_max) const {
     int verts[4];
-    int vert_min = get_elem_verts(elem, verts);
+    get_cell_verts(cell, verts);
 
     get_vert_xloc(verts[0], xloc_min);
     get_vert_xloc(verts[2], xloc_max);
@@ -95,52 +117,61 @@ class GDMesh2D final : public MeshBase<T, 2, Np_1d * Np_1d, 4> {
   using MeshBase::nodes_per_element;
   using MeshBase::spatial_dim;
 
-  GDMesh2D(const Grid& grid) : grid(grid) {
-    const int* nxy = grid.get_nxy();
-    for (int d = 0; d < spatial_dim; d++) {
-      if (nxy[d] < Np_1d - 1) {
-        char msg[256];
-        std::snprintf(
-            msg, 256,
-            "too few elements (%d) for Np_1d (%d) along %d-th dimension",
-            nxy[d], Np_1d, d);
-        throw std::runtime_error(msg);
-      }
-    }
+  /**
+   * @brief Construct GD Mesh given grid only
+   */
+  GDMesh2D(const Grid& grid) : grid(grid), has_lsf(false) {
+    check_grid_compatibility(grid);
+    num_nodes = grid.get_num_verts();
+    num_elements = grid.get_num_cells();
   }
 
-  int get_num_nodes() const {
-    const int* nxy = grid.get_nxy();
-    int nnodes = 1;
-    for (int d = 0; d < spatial_dim; d++) {
-      nnodes *= nxy[d] + 1;
-    }
-    return nnodes;
+  /**
+   * @brief Construct GD Mesh given grid and LSF function
+   *
+   * @tparam Func functor type
+   * @param grid GD grid
+   * @param lsf level set function that determining the analysis domain, lsf(T*
+   * xyz) where xyz contains x,y,(z) coordinates.
+   *
+   * Note: Within the analysis domain, lsf <= 0
+   */
+  template <class Func>
+  GDMesh2D(const Grid& grid, const Func& lsf) : grid(grid), has_lsf(true) {
+    check_grid_compatibility(grid);
+    init_dofs_from_lsf(lsf);
+    num_nodes = dof_nodes.size();
+    num_elements = dof_elems.size();
   }
 
-  int get_num_elements() const {
-    const int* nxy = grid.get_nxy();
-    int nelems = 1;
-    for (int d = 0; d < spatial_dim; d++) {
-      nelems *= nxy[d];
-    }
-    return nelems;
-  }
+  int get_num_nodes() const { return num_nodes; }
+  int get_num_elements() const { return num_elements; }
 
   inline void get_node_xloc(int node, T* xloc) const {
     grid.get_vert_xloc(node, xloc);
   }
 
   /**
-   * @brief Get the stencil nodes given a gd element (i.e. a grid cell)
+   * @brief For a GD element, get all dof nodes
    *
    * @param elem element index
    * @param nodes dof node indices, length: nodes_per_element
    */
   void get_elem_dof_nodes(int elem, int* nodes) const {
+    get_elem_ground_stencil(elem, nodes);
+    if (has_lsf) {
+      adjust_stencil(elem, nodes);
+    }
+  }
+
+  /**
+   * @brief For a GD element, get the stencil associated to the ground grid,
+   * regardless the optional boundary defined by the level set function
+   */
+  void get_elem_ground_stencil(int elem, int* nodes) const {
     constexpr int q = Np_1d / 2;
     int eij[spatial_dim];
-    grid.get_elem_coords(elem, eij);
+    grid.get_cell_coords(elem, eij);
     const int* nxy = grid.get_nxy();
     for (int d = 0; d < spatial_dim; d++) {
       if (eij[d] < q - 1) {
@@ -159,8 +190,13 @@ class GDMesh2D final : public MeshBase<T, 2, Np_1d * Np_1d, 4> {
     }
   };
 
+  /**
+   * @brief Adjust the stencil such that all nodes are active nodes
+   */
+  void adjust_stencil(int elem, int* nodes) const {}  // TODO
+
   inline void get_elem_dof_verts(int elem, int* verts) const {
-    grid.get_elem_verts(elem, verts);
+    grid.get_cell_verts(elem, verts);
   }
 
   void get_elem_node_ranges(int elem, T* xloc_min, T* xloc_max) const {
@@ -192,11 +228,82 @@ class GDMesh2D final : public MeshBase<T, 2, Np_1d * Np_1d, 4> {
   }
 
   inline void get_elem_vert_ranges(int elem, T* xloc_min, T* xloc_max) const {
-    grid.get_elem_vert_ranges(elem, xloc_min, xloc_max);
+    grid.get_cell_vert_ranges(elem, xloc_min, xloc_max);
   }
 
  private:
+  void check_grid_compatibility(const Grid& grid) const {
+    const int* nxy = grid.get_nxy();
+    for (int d = 0; d < spatial_dim; d++) {
+      if (nxy[d] < Np_1d - 1) {
+        char msg[256];
+        std::snprintf(
+            msg, 256,
+            "too few elements (%d) for Np_1d (%d) along %d-th dimension",
+            nxy[d], Np_1d, d);
+        throw std::runtime_error(msg);
+      }
+    }
+  }
+
+  template <class Func>
+  void init_dofs_from_lsf(const Func& lsf) {
+    // LSF values are always associated with the ground grid verts, unlike the
+    // dof values which might only be associated with part of the ground grid
+    // verts (i.e. nodes)
+    int nverts = grid.get_num_verts();
+    std::vector<T> lsf_dof(nverts);
+
+    // Populate lsf values
+    std::vector<bool> active_lsf_verts(nverts, false);
+    for (int i = 0; i < nverts; i++) {
+      T xloc[spatial_dim];
+      grid.get_vert_xloc(i, xloc);
+      lsf_dof[i] = lsf(xloc);
+      if (lsf_dof[i] <= T(0.0)) {
+        active_lsf_verts[i] = true;
+      }
+    }
+
+    // Active cell is a cell with at least one active lsf vert
+    int ncells = grid.get_num_cells();
+    std::vector<bool> active_cells(ncells, false);
+
+    // Unlike LSF values, dof are associated with nodes, which is a subset of
+    // verts. Here we determine which verts are dof nodes
+    for (int c = 0; c < ncells; c++) {
+      if (active_cells[c]) continue;
+      int verts[Grid::nverts_per_cell];
+      grid.get_cell_verts(c, verts);
+      for (int i = 0; i < Grid::nverts_per_cell; i++) {
+        if (active_lsf_verts[verts[i]]) {
+          active_cells[c] = true;
+          break;
+        }
+      }
+    }
+
+    // Create active dof nodes and the mapping to verts
+    for (int c = 0; c < ncells; c++) {
+      if (!active_cells[c]) continue;
+      int verts[Grid::nverts_per_cell];
+      grid.get_cell_verts(c, verts);
+      for (int i = 0; i < Grid::nverts_per_cell; i++) {
+        dof_nodes_set.insert(verts[i]);
+      }
+    }
+
+    dof_nodes.resize(dof_nodes_set.size());
+    std::copy(dof_nodes_set.begin(), dof_nodes_set.end(), dof_nodes.begin());
+  }
+
   const Grid& grid;
+  int num_nodes = -1;
+  int num_elements = -1;
+  bool has_lsf = false;
+  std::vector<int> dof_nodes;  // nodes that have active degrees of freedom
+  std::vector<int> dof_elems;  // elements that have active degrees of freedom
+  std::set<int> dof_nodes_set;
 };
 
 #endif  // XCGD_GD_COMMONS_H
---
+++ tests/mesh/test_gd_mesh.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+
+#include "elements/gd_vandermonde.h"
+#include "test_commons.h"
+#include "utils/vtk.h"
+
+TEST(GDMeshTest, Structured) {
+  constexpr int Np_1d = 4;
+  using T = double;
+  using Grid = StructuredGrid2D<T>;
+  using Mesh = GDMesh2D<T, Np_1d>;
+
+  int nxy[2] = {4, 3};
+  T lxy[2] = {1.0, 1.0};
+  Grid grid(nxy, lxy);
+  Mesh mesh(grid);
+
+  std::vector<int> nodes1 = {0,  1,  2,  3,  5,  6,  7,  8,
+                             10, 11, 12, 13, 15, 16, 17, 18};
+  std::vector<int> nodes2 = {1,  2,  3,  4,  6,  7,  8,  9,
+                             11, 12, 13, 14, 16, 17, 18, 19};
+
+  for (int elem = 0; elem < 12; elem++) {
+    int nodes[mesh.nodes_per_element];
+    mesh.get_elem_dof_nodes(elem, nodes);
+    int eij[2];
+    grid.get_cell_coords(elem, eij);
+    if (eij[0] < 2) {
+      EXPECT_VEC_EQ(nodes1.size(), nodes, nodes1);
+    } else {
+      EXPECT_VEC_EQ(nodes2.size(), nodes, nodes2);
+    }
+  }
+}
+
+// TEST(GDMeshTest, LSF) {
+//   constexpr int Np_1d = 4;
+//   using T = double;
+//   using Grid = StructuredGrid2D<T>;
+//   using Mesh = GDMesh2D<T, Np_1d>;
+
+//   int nxy[2] = {20, 20};
+//   T lxy[2] = {1.0, 1.0};
+
+//   auto lsf = [](T xy[2]) {
+//     T x = xy[0];
+//     T y = xy[1];
+//     return (x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5) - 0.25;
+//   };
+
+//   Grid grid(nxy, lxy);
+//   Mesh mesh(grid, lsf);
+
+//   std::vector<T> active_lsf_verts(grid.get_num_verts(), 0.0);
+//   std::vector<T> active_dof_verts(grid.get_num_verts(), 0.0);
+
+//   for (int i = 0; i < grid.get_num_verts(); i++) {
+//     if (mesh.active_lsf_verts[i]) {
+//       active_lsf_verts[i] = 1.0;
+//     }
+//   }
+
+//   for (int vert : mesh.active_dof_nodes) {
+//     active_dof_verts[vert] = 1.0;
+//   }
+
+//   ToVTK<T, Mesh> vtk(mesh, "mesh_gd.vtk");
+
+//   vtk.write_mesh();
+//   vtk.write_sol("active_lsf_nodes", active_lsf_verts.data());
+//   vtk.write_sol("active_dof_nodes", active_dof_verts.data());
+// }
