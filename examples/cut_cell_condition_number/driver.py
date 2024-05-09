4dc6ca6ffa15f5380bc3e4d1e00f838bf0c1e982
Yicong Fu <fuyicong1996@gmail.com>
2024-05-09 11:47:57 -0400

    minor update

--- examples/cut_cell_condition_number/driver.py
+++ examples/cut_cell_condition_number/driver.py
@@ -13,13 +13,14 @@ def sweep(ax, Np_1d=2, exp_max=-1.0, exp_min=-5.0, num_pts=10, n=33):
     l = 1.0
     h = l / n
     r0_max = h * 2.0**0.5
+    p = Np_1d - 1
 
     delta = []
     cond = []
 
     for i, d in enumerate(tqdm(np.logspace(exp_max, exp_min, num_pts))):
         r = r0_max - d * h
-        prefix = "driver_output_%d" % i
+        prefix = "cond_study_p_%d_%d" % (p, i)
         cmd = [
             "./condition_number",
             "--Np_1d=%d" % Np_1d,
@@ -37,9 +38,7 @@ def sweep(ax, Np_1d=2, exp_max=-1.0, exp_min=-5.0, num_pts=10, n=33):
             np.linalg.cond(mmread(path.join(prefix, "stiffness_matrix.mtx")).todense())
         )
 
-    ax.loglog(
-        delta, cond, "-o", label=("p=%d" % (Np_1d - 1)), clip_on=False, zorder=100
-    )
+    ax.loglog(delta, cond, "-o", label=("p=%d" % p), clip_on=False, zorder=100)
 
     return
 
@@ -54,4 +53,4 @@ if __name__ == "__main__":
 
     ax.invert_xaxis()
     ax.legend()
-    plt.show()
+    plt.savefig("condition_number_study.pdf")
