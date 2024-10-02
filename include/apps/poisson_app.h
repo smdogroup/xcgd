b916d4e9f25ce35276e1eedcad18fb3fd5e841eb
Yicong Fu <fuyicong1996@gmail.com>
2024-10-01 22:57:52 -0400

    add poisson app (together with the previous commit)

---
+++ include/apps/poisson_app.h
@@ -0,0 +1,141 @@
+#include "analysis.h"
+#include "physics/poisson.h"
+#include "sparse_utils/sparse_utils.h"
+#include "utils/misc.h"
+
+#pragma once
+
+template <typename T, class Mesh, class Quadrature, class Basis>
+class PoissonApp final {
+ public:
+  using Physics = PoissonPhysics<T, Basis::spatial_dim>;
+
+ private:
+  using Analysis = GalerkinAnalysis<T, Mesh, Quadrature, Basis, Physics>;
+  using BSRMat = GalerkinBSRMat<T, Physics::dof_per_node>;
+  using CSCMat = SparseUtils::CSCMat<T>;
+
+ public:
+  PoissonApp(Mesh& mesh, Quadrature& quadrature, Basis& basis, T source = 1.0)
+      : mesh(mesh),
+        quadrature(quadrature),
+        basis(basis),
+        physics(source),
+        analysis(mesh, quadrature, basis, physics) {}
+
+  ~PoissonApp() = default;
+
+  // Compute Jacobian matrix with boundary conditions
+  BSRMat* jacobian(const std::vector<int>& bc_dof) {
+    int ndof = Physics::dof_per_node * mesh.get_num_nodes();
+
+    // Set up Jacobian matrix
+    int *rowp = nullptr, *cols = nullptr;
+    auto& mesh = this->mesh;
+    SparseUtils::CSRFromConnectivityFunctor(
+        mesh.get_num_nodes(), mesh.get_num_elements(),
+        mesh.max_nnodes_per_element,
+        [&mesh](int elem, int* nodes) -> int {
+          return mesh.get_elem_dof_nodes(elem, nodes);
+        },
+        &rowp, &cols);
+
+    int nnz = rowp[mesh.get_num_nodes()];
+    BSRMat* jac_bsr = new BSRMat(mesh.get_num_nodes(), nnz, rowp, cols);
+
+    // Compute Jacobian matrix
+    std::vector<T> zeros(ndof, 0.0);
+    analysis.jacobian(nullptr, zeros.data(), jac_bsr);
+
+    // Apply bcs
+    jac_bsr->zero_rows(bc_dof.size(), bc_dof.data());
+
+    if (rowp) delete rowp;
+    if (cols) delete cols;
+
+    return jac_bsr;
+  }
+
+  std::vector<T> solve(const std::vector<int>& bc_dof,
+                       const std::vector<T>& bc_vals) {
+    int ndof = Physics::dof_per_node * mesh.get_num_nodes();
+
+    // Compute Jacobian matrix
+    BSRMat* jac_bsr = jacobian(bc_dof);
+    CSCMat* jac_csc = SparseUtils::bsr_to_csc(jac_bsr);
+    jac_csc->zero_columns(bc_dof.size(), bc_dof.data());
+
+    // Set the right hand side
+    std::vector<T> rhs(ndof, 0.0), t1(ndof, 0.0), t2(ndof, 0.0);
+
+    analysis.residual(nullptr, t1.data(), rhs.data());
+    for (int i = 0; i < rhs.size(); i++) {
+      rhs[i] *= -1.0;
+    }
+    for (int i = 0; i < bc_dof.size(); i++) {
+      rhs[bc_dof[i]] = bc_vals[i];
+    }
+
+    for (int i = 0; i < bc_dof.size(); i++) {
+      t1[bc_dof[i]] = bc_vals[i];
+    }
+
+    jac_bsr->axpy(t1.data(), t2.data());
+    for (int i = 0; i < bc_dof.size(); i++) {
+      t2[bc_dof[i]] = 0.0;
+    }
+
+    for (int i = 0; i < rhs.size(); i++) {
+      t2[i] = rhs[i] - t2[i];
+    }
+
+    // Factorize Jacobian matrix
+    SparseUtils::CholOrderingType order = SparseUtils::CholOrderingType::ND;
+    SparseUtils::SparseCholesky<T>* chol =
+        new SparseUtils::SparseCholesky<T>(jac_csc);
+    chol->factor();
+    std::vector<T> sol = t2;
+    chol->solve(sol.data());
+
+#ifdef XCGD_DEBUG_MODE
+    // Write Jacobian matrix to a file
+    jac_csc->write_mtx("K_bcs.mtx");
+
+    ToVTK<T, Mesh> vtk(mesh, "debug.vtk");
+    vtk.write_mesh();
+    vtk.write_sol("rhs", rhs.data());
+
+    // Check error
+    // res = Ku - rhs
+    std::vector<T> Ku(sol.size());
+    jac_bsr->axpy(sol.data(), Ku.data());
+    T err = 0.0;
+    T rhs2 = 0.0;
+    for (int i = 0; i < Ku.size(); i++) {
+      err += (Ku[i] - rhs[i]) * (Ku[i] - rhs[i]);
+      rhs2 += rhs[i] * rhs[i];
+    }
+    std::printf("[Debug]Poisson residual:\n");
+    std::printf("||Ku - f||_2 / ||f||_2: %25.15e\n", sqrt(err) / sqrt(rhs2));
+#endif
+
+    if (jac_bsr) delete jac_bsr;
+    if (jac_csc) delete jac_csc;
+    if (chol) delete chol;
+
+    return sol;
+  }
+
+  Mesh& get_mesh() { return mesh; }
+  Quadrature& get_quadrature() { return quadrature; }
+  Basis& get_basis() { return basis; }
+  Analysis& get_analysis() { return analysis; }
+
+ private:
+  Mesh& mesh;
+  Quadrature& quadrature;
+  Basis& basis;
+
+  Physics physics;
+  Analysis analysis;
+};
