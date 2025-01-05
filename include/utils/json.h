009a1d1f069c3ecdea28fb47b7f7963eb02b744e
Yicong Fu <fuyicong1996@gmail.com>
2025-01-04 22:19:36 -0500

    inline pure json functions

--- include/utils/json.h
+++ include/utils/json.h
@@ -7,12 +7,12 @@
 
 using json = nlohmann::json;
 
-void write_json(std::string json_path, const json& json_obj) {
+inline void write_json(std::string json_path, const json& json_obj) {
   std::ofstream o(json_path);
   o << std::setw(4) << json_obj << std::endl;
 };
 
-json read_json(std::string json_path) {
+inline json read_json(std::string json_path) {
   std::ifstream i(json_path);
   json j;
   i >> j;
