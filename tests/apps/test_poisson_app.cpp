55f91e064b279142a6bc7501c62ce40d9076698e
Yicong Fu <fuyicong1996@gmail.com>
2025-02-20 13:54:21 -0500

    added a poisson app using nitsche bcs

--- tests/apps/test_poisson_app.cpp
+++ tests/apps/test_poisson_app.cpp
@@ -1,3 +1,6 @@
+#include <memory>
+#include <type_traits>
+
 #include "apps/poisson_app.h"
 #include "elements/gd_mesh.h"
 #include "elements/gd_vandermonde.h"
@@ -10,9 +13,25 @@ void expect_sol_near(T xmin, T xmax, T ymin, T ymax, Mesh &mesh,
   auto source_fun = [](const A2D::Vec<T, Basis::spatial_dim> &xloc) {
     return T(0.0);
   };
-  using Poisson = PoissonApp<T, Mesh, Quadrature, Basis, typeof(source_fun)>;
+  auto exact_fun = [](const A2D::Vec<T, Basis::spatial_dim> &xloc) {
+    return 2.0 * (1.0 + xloc[1]) /
+           ((3.0 + xloc[0]) * (3.0 + xloc[0]) +
+            (1.0 + xloc[1]) * (1.0 + xloc[1]));
+  };
 
-  Poisson poisson(mesh, quadrature, basis, source_fun);
+  using Poisson = typename std::conditional<
+      Mesh::is_cut_mesh,
+      PoissonNitscheApp<T, Mesh, Quadrature, Basis, typeof(source_fun),
+                        typeof(exact_fun)>,
+      PoissonApp<T, Mesh, Quadrature, Basis, typeof(source_fun)>>::type;
+
+  std::shared_ptr<Poisson> poisson;
+  if constexpr (Mesh::is_cut_mesh) {
+    poisson = std::make_shared<Poisson>(mesh, quadrature, basis, source_fun,
+                                        exact_fun, 1e6);
+  } else {
+    poisson = std::make_shared<Poisson>(mesh, quadrature, basis, source_fun);
+  }
 
   int nnodes = mesh.get_num_nodes();
 
@@ -31,14 +50,18 @@ void expect_sol_near(T xmin, T xmax, T ymin, T ymax, Mesh &mesh,
                          ((3.0 + xloc[0]) * (3.0 + xloc[0]) +
                           (1.0 + xloc[1]) * (1.0 + xloc[1])));
     }
-    sol_exact[i] =
-        2.0 * (1.0 + xloc[1]) /
-        ((3.0 + xloc[0]) * (3.0 + xloc[0]) + (1.0 + xloc[1]) * (1.0 + xloc[1]));
+    sol_exact[i] = exact_fun(xloc);
   }
 
-  std::vector<T> sol = poisson.solve(dof_bcs, dof_vals);
+  std::vector<T> sol;
+  if constexpr (Mesh::is_cut_mesh) {
+    sol = poisson->solve();
+  } else {
+    sol = poisson->solve(dof_bcs, dof_vals);
+  }
 
-  ToVTK<T, Mesh> vtk(mesh, "poisson.vtk");
+  ToVTK<T, Mesh> vtk(mesh, std::string("poisson") +
+                               (Mesh::is_cut_mesh ? "_nitsche" : "") + ".vtk");
   vtk.write_mesh();
   vtk.write_sol("u", sol.data());
 
@@ -70,4 +93,22 @@ void test_poisson_app() {
   expect_sol_near(-1.0, 1.0, -1.0, 1.0, mesh, quadrature, basis);
 }
 
-TEST(apps, Poisson) { test_poisson_app<2>(); }
+template <int Np_1d>
+void test_poisson_app_nitsche() {
+  using T = double;
+  using Grid = StructuredGrid2D<T>;
+  using Quadrature = GDLSFQuadrature2D<T, Np_1d>;
+  using Mesh = CutMesh<T, Np_1d>;
+  using Basis = GDBasis2D<T, Mesh>;
+  int nxy[2] = {32, 32};
+  T lxy[2] = {2.0, 2.0};
+  T xy0[2] = {-1.0, -1.0};
+  Grid grid(nxy, lxy, xy0);
+  Mesh mesh(grid, [](T *x) { return x[0] * x[0] + x[1] * x[1] - 0.8 * 0.8; });
+  Quadrature quadrature(mesh);
+  Basis basis(mesh);
+  expect_sol_near(-1.0, 1.0, -1.0, 1.0, mesh, quadrature, basis);
+}
+
+TEST(apps, Poisson) { test_poisson_app<4>(); }
+TEST(apps, PoissonNitsche) { test_poisson_app_nitsche<4>(); }
