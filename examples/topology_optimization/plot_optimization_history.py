8a6f674e8446c4eb5304016a6e142d490e7516f2
Yicong Fu <fuyicong1996@gmail.com>
2025-04-03 15:18:22 -0400

    update history plotter

--- examples/topology_optimization/plot_optimization_history.py
+++ examples/topology_optimization/plot_optimization_history.py
@@ -2,10 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
+import os
+import scienceplots
+
+# # Get ggplot colors
+colors = plt.style.library["ggplot"]["axes.prop_cycle"].by_key()["color"]
+
+plt.style.use(["science"])
 
 p = argparse.ArgumentParser()
 p.add_argument("logfile", type=str)
-p.add_argument("--columns", nargs="+", default=["comp"])
 
 args = p.parse_args()
 
@@ -13,21 +19,39 @@ with open(args.logfile, "r") as f:
     df = pd.read_csv(f, delimiter=r"\s+", skip_blank_lines=True)
 
 # Remove extra header lines
-df = df[df["iter"] != "iter"]
+df = df[df["major"] != "major"]
 df = df.apply(lambda col: pd.to_numeric(col, errors="ignore"))
 
-df["group"] = (df["iter"] == 0).cumsum()
+df["group"] = (df["major"] == 0).cumsum()
 df = df[df["group"] == df["group"].max()]
 df = df.drop(columns=["group"])
 df = df.reset_index(drop=True)
 
 
-fig, ax = plt.subplots()
-for col in args.columns:
-    ax.plot(df["iter"], df[col], label=col)
+fig, ax = plt.subplots(figsize=(4.8, 3.6))
 
-ax.legend()
-ax.grid()
-ax.set_xlabel("Iter")
+obj_color_index = 2
+ax.plot(
+    df["major"],
+    df["obj"],
+    "-",
+    lw=1.0,
+    color=colors[obj_color_index],
+)
 
-plt.show()
+# vol_color_index = 3
+# ax2 = ax.twinx()
+# ax2.plot(
+#     df["major"],
+#     df[r"vol(%)"],
+#     "-",
+#     lw=1.0,
+#     color=colors[vol_color_index],
+# )
+# ax2.set_ylabel(r"Volume (\%)")
+
+ax.set_xlabel("Optimization Iteration")
+ax.set_ylabel("Objective Value")
+
+
+plt.savefig(os.path.join(os.path.dirname(args.logfile), "optimization_history.svg"))
